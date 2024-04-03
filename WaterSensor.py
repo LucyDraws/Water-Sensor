41176576e770a8e0c239773882c5e097cdc0150d
LucyDraws <162621567+LucyDraws@users.noreply.github.com>
2024-04-03 13:38:43 -0400

    Create WaterSensor.py

---
+++ WaterSensor.py
@@ -0,0 +1,15 @@
+int readPin = A0;
+int readValue;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(readPin, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readValue = analogRead(readPin);
+  Serial.println(readValue);
+  delay(1000);
+}
